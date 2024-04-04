fd0083766803090312c4e500c7f80309c4abe554
Igor Henrique Lorenço <127193871+igorhnq@users.noreply.github.com>
2024-04-04 16:55:51 -0300

    componentization

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,4 +1,9 @@
 import {Search, MoreHorizontal, ChevronsLeft, ChevronLeft, ChevronRight, ChevronsRight} from 'lucide-react'
+import { IconButton } from './icon-button'
+import { Table } from './table/table'
+import { TableHeader } from './table/table-header'
+import { TableCell } from './table/table-cell'
+import { TableRow } from './table/table-row'
 
 export function AttendeeList() {
     return (
@@ -10,77 +15,67 @@ export function AttendeeList() {
                     <input className="bg-transparent flex-1 outline-none h-auto p-0 border-0 text-sm" placeholder="Buscar um participante..."/>
                 </div>
             </div>
-
-
-            <div className="border border-white/10 rounded-lg">
-                <table className="w-full">
+            <Table>
                     <thead>
-                        <tr className="border-b border-white/10 ">
-                            <th className="py-3 px-4 text-sm font-semibold text-left">
+                        <tr className="border-b border-white/10">
+                            <TableHeader>
                                 <input className="size-4 bg-black/20 rounded border border-white/10" type="checkbox"/>
-                            </th>
-                            <th style={{width: 48}}className="py-3 px-4 text-sm font-semibold text-left">Codigo</th>
-                            <th className="py-3 px-4 text-sm font-semibold text-left">Participante</th>
-                            <th className="py-3 px-4 text-sm font-semibold text-left">Data de inscricao</th>
-                            <th className="py-3 px-4 text-sm font-semibold text-left">Data do check-in</th>
-                            <th style={{width: 64}} className="py-3 px-4 text-sm font-semibold text-left"></th>
+                            </TableHeader>
+                            <TableHeader style={{width: 48}}>Codigo</TableHeader>
+                            <TableHeader>Participante</TableHeader>
+                            <TableHeader>Data de inscricao</TableHeader>
+                            <TableHeader>Data do check-in</TableHeader>
+                            <TableHeader style={{width: 64}}></TableHeader>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="border-b border-white/10 hover:bg-white/5">
-                            <td className="py-3 px-4 text-sm text-zinc-300">
+                        <TableRow className="border-b border-white/10 hover:bg-white/5">
+                            <TableCell>
                                 <input className="size-4 bg-black/20 rounded border border-white/10" type="checkbox"/>
-                            </td>
-                            <td className="py-3 px-4 text-sm text-zinc-300">12343</td>
-                            <td className="py-3 px-4 text-sm text-zinc-300">
+                            </TableCell>
+                            <TableCell>12343</TableCell>
+                            <TableCell>
                                 <div className="flex flex-col gap-1">
                                     <span className="font-semibold text-white">Diego Schell Fernandez</span>
                                     <span>diego@rocketseat.com.br</span>
                                 </div>
-                            </td>
-                            <td className="py-3 px-4 text-sm text-zinc-300">7 dias atras</td>
-                            <td className="py-3 px-4 text-sm text-zinc-300">3 dias atras</td>
-                            <td className="py-3 px-4 text-sm text-zinc-300">
-                                <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                            </TableCell>
+                            <TableCell>7 dias atras</TableCell>
+                            <TableCell>3 dias atras</TableCell>
+                            <TableCell>
+                                <IconButton transparent>
                                     <MoreHorizontal className="size-4"/>
-                                </button>
-                            </td>
-                        </tr>
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
                     </tbody>
                     <tfoot>
                         <tr>
-                            <td className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
+                            <TableCell colSpan={3}>
                                 Mostrando 10 de 228 itens
-                            </td>
-                            <td className="py-3 px-4 text-sm text-zinc-300 text-right" colSpan={3}>
-                        
-
+                            </TableCell>
+                            <TableCell className="text-right" colSpan={3}>
                                <div className="inline-flex items-center gap-8">
                                     <span>Pagina 1 de 23</span>
                                     <div className="flex gap-1.5">
-                                        <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                                        <IconButton>
                                             <ChevronsLeft className="size-4"/>
-                                        </button>
-                                        <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                                        </IconButton>
+                                        <IconButton>
                                             <ChevronLeft className="size-4"/>
-                                        </button>
-                                        <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                                        </IconButton>
+                                        <IconButton>
                                             <ChevronRight className="size-4"/>
-                                        </button>
-                                        <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                                        </IconButton>
+                                        <IconButton>
                                             <ChevronsRight className="size-4"/>
-                                        </button>
+                                        </IconButton>
                                     </div>
                                </div>
-
-
-                            </td>
+                            </TableCell>
                         </tr>
                     </tfoot>
-                </table>
-            </div>
+            </Table>
         </div>
-        
-       
     )
 }
